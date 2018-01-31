fd84f606d214e6cd984d32046b5cf2249574d239
Arnaud Botella <arnaud.botella@univ-lorraine.fr>
2018-01-31 21:18:46 +0100

    first commit

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.css'
 
 import App from './App'
 import store from './store'
 
+Vue.use(Vuetify)
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.config.productionTip = false
 
