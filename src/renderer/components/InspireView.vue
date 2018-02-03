599930b10f16dbfd368478c29c528f322a7e97cb
Arnaud Botella <arnaud.botella@univ-lorraine.fr>
2018-02-03 18:13:26 +0100

    first try

--- src/renderer/components/InspireView.vue
+++ src/renderer/components/InspireView.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-layout column justify-center>
+  <v-layout column justify-center class="text-xs-center">
     <v-flex>
       <img src="/static/v.png" alt="Vuetify.js" class="mb-5" />
       <blockquote class="text-xs-center">
@@ -10,9 +10,26 @@
           </small>
         </footer>
       </blockquote>
+      <v-btn color="primary" @click.native="load">Load GM3D</v-btn>
     </v-flex>
   </v-layout>
 </template>
+
+<script>
+  export default {
+    methods: {
+      load () {
+        const remote = require('electron').remote
+        const geomodelCore = require('/home/botella/RING/RINGMesh/build/Debug/ringmesh/node/ringmesh/geomodel_core').init().lib
+        let gm = new geomodelCore.GeoModel3D()
+        const file = remote.dialog.showOpenDialog({properties: ['openFile']})
+        console.log(file)
+        const io = require('/home/botella/RING/RINGMesh/build/Debug/ringmesh/node/ringmesh/RINGMesh_io').init().lib
+        io.geomodel_load3D(gm, '/home/botella/RING/RINGMesh/data/modelA1.ml')
+      }
+    }
+  }
+</script>
 
 <style scoped>
   img {
